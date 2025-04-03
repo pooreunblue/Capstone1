808f07b0b23878c04fe91e4abfbb9b70c973f11a
baekseungho <mjbsh02@mju.ac.kr>
2025-04-03 20:50:33 +0900

    Feat: Device 엔티티 생성

---
+++ capstone1/src/main/java/org/vox/capstonedesign1/domain/Device.java
@@ -0,0 +1,24 @@
+package org.vox.capstonedesign1.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Device {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "device_name", nullable = false)
+    private String deviceName;
+}
