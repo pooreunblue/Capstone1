0c54071b0dd4245486d1a13a00a103eb9a96ed96
baekseungho <mjbsh02@mju.ac.kr>
2025-04-06 16:22:12 +0900

    Feat: AgentSignalRepository 커스텀 기능 정의

--- capstone1/src/main/java/org/vox/capstonedesign1/repository/AgentSignalRepository.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/repository/AgentSignalRepository.java
@@ -1,3 +1,14 @@
 package org.vox.capstonedesign1.repository;
 
-public interface AgentSignalRepository{}
+import org.vox.capstonedesign1.domain.AgentSignal;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AgentSignalRepository{
+    AgentSignal save(AgentSignal signal);
+    Optional<AgentSignal> findById(Long id);
+    Optional<AgentSignal> findByDeviceId(int deviceId);
+    // 데이터를 최신시간순으로 정렬해서 가져오기
+    List<AgentSignal> findAllByOrderByTimeStampDesc();
+}
