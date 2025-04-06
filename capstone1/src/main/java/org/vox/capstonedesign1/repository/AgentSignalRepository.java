8042619f0f9b7cfcf27175f60c06b31f317c82d2
baekseungho <mjbsh02@mju.ac.kr>
2025-04-06 18:56:59 +0900

    Feat: AgentSignalRepository Jpa 접근방식으로 수정

--- capstone1/src/main/java/org/vox/capstonedesign1/repository/AgentSignalRepository.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/repository/AgentSignalRepository.java
@@ -4,13 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.vox.capstonedesign1.domain.AgentSignal;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface AgentSignalRepository extends JpaRepository<AgentSignal, Long> {
 //  CrudRepository에 구현되어있음
 //  AgentSignal save(AgentSignal signal);
 //  Optional<AgentSignal> findById(Long id);
-    List<AgentSignal> findAllByDeviceId(int deviceId);
+    List<AgentSignal> findAllByDevice_Id(int deviceId);
     // 데이터를 최신시간순으로 정렬해서 가져오기
     List<AgentSignal> findAllByOrderByTimeStampDesc();
 }
