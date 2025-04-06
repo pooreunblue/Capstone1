4a2ac5b1082e63ae71cfd0d455c8c9620d4e5a10
baekseungho <mjbsh02@mju.ac.kr>
2025-04-06 16:27:36 +0900

    Feat: 구현체 AgentSignalRepositoryImpl 구현, implement 메소드

--- capstone1/src/main/java/org/vox/capstonedesign1/repository/AgentSignalRepositoryImpl.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/repository/AgentSignalRepositoryImpl.java
@@ -1,3 +1,28 @@
 package org.vox.capstonedesign1.repository;
 
-public class AgentSignalRepositoryImpl {}
+import org.vox.capstonedesign1.domain.AgentSignal;
+
+import java.util.List;
+import java.util.Optional;
+
+public class AgentSignalRepositoryImpl implements AgentSignalRepository {
+    @Override
+    public AgentSignal save(AgentSignal signal) {
+        return null;
+    }
+
+    @Override
+    public Optional<AgentSignal> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<AgentSignal> findByDeviceId(int deviceId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<AgentSignal> findAllByOrderByTimeStampDesc() {
+        return List.of();
+    }
+}
