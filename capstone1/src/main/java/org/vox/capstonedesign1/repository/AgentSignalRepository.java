5410c71f07d8c0f7eff0e1282f8ecebef1c27493
baekseungho <mjbsh02@mju.ac.kr>
2025-04-06 17:52:25 +0900

    Feat: AgentSignalRepository JpaRepository 상속

--- capstone1/src/main/java/org/vox/capstonedesign1/repository/AgentSignalRepository.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/repository/AgentSignalRepository.java
@@ -1,11 +1,12 @@
 package org.vox.capstonedesign1.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.vox.capstonedesign1.domain.AgentSignal;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface AgentSignalRepository{
+public interface AgentSignalRepository extends JpaRepository<AgentSignal, Long> {
     AgentSignal save(AgentSignal signal);
     Optional<AgentSignal> findById(Long id);
     Optional<AgentSignal> findByDeviceId(int deviceId);
