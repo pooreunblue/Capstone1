265472a3b5fb2f31e9cd962f8467df729af3b9ce
baekseungho <mjbsh02@mju.ac.kr>
2025-04-06 18:32:08 +0900

    Feat: AgentSignalRepository 사용자 정의 기능 수정

--- capstone1/src/main/java/org/vox/capstonedesign1/repository/AgentSignalRepository.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/repository/AgentSignalRepository.java
@@ -7,9 +7,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface AgentSignalRepository extends JpaRepository<AgentSignal, Long> {
-    AgentSignal save(AgentSignal signal);
-    Optional<AgentSignal> findById(Long id);
-    Optional<AgentSignal> findByDeviceId(int deviceId);
+//  CrudRepository에 구현되어있음
+//  AgentSignal save(AgentSignal signal);
+//  Optional<AgentSignal> findById(Long id);
+    List<AgentSignal> findAllByDeviceId(int deviceId);
     // 데이터를 최신시간순으로 정렬해서 가져오기
     List<AgentSignal> findAllByOrderByTimeStampDesc();
 }
