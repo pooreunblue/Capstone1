2fdce82e244b4b2511451e4c8830b8273d2151ba
baekseungho <mjbsh02@mju.ac.kr>
2025-04-12 12:18:10 +0900

    Refactor: 예외 수정

--- capstone1/src/main/java/org/vox/capstonedesign1/service/UdpSocketConfig.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/service/UdpSocketConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.net.DatagramSocket;
+import java.net.SocketException;
 
 @Configuration
 public class UdpSocketConfig {
 
     @Bean
-    public DatagramSocket datagramSocket() throws Exception {
+    public DatagramSocket datagramSocket() throws SocketException {
         return new DatagramSocket(9999);
     }
 }
