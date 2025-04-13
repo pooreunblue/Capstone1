8864fcb16378dbf55a8997ccabce5597baf6326e
baekseungho <mjbsh02@mju.ac.kr>
2025-04-13 13:27:37 +0900

    Refactor: MockDatagramSocket 시뮬레이션 방식 수정

--- capstone1/src/test/java/org/vox/capstonedesign1/domain/UdpReceiverIntegrationTest.java
+++ capstone1/src/test/java/org/vox/capstonedesign1/domain/UdpReceiverIntegrationTest.java
@@ -1,34 +1,29 @@
 package org.vox.capstonedesign1.domain;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.vox.capstonedesign1.dto.AgentSignalRequest;
 import org.vox.capstonedesign1.service.AgentSignalService;
 import org.vox.capstonedesign1.testutil.MockDatagramSocket;
 
 import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.mockito.Mockito.*;
 
-class UdpReceiverSocketTest {
+class UdpReceiverIntegrationTest {
 
     @Test
-    void Udp통합테스트() throws Exception {
-
-        // Given
-        AgentSignalService agentSignalService = mock(AgentSignalService.class);
-        UdpReceiver receiver = new UdpReceiver(agentSignalService);
-        String json = "{\"deviceId\":42,\"statusId\":\"2\", \"timeStamp\": \"2025-04-12T12:48:00\"}";
-        byte[] testData = json.getBytes("UTF-8");
-        MockDatagramSocket mockSocket = new MockDatagramSocket(testData);
-        receiver.setSocketSupplier(() -> mockSocket);
-
-        // when
+    void UDP소켓서비스전달테스트() throws Exception {
+        AgentSignalService mockService = mock(AgentSignalService.class);
+        MockDatagramSocket mockDatagramSocket = new MockDatagramSocket();
+        UdpReceiver receiver = new UdpReceiver(mockService, mockDatagramSocket);
         receiver.startUdpServer();
-        Thread.sleep(200); // 너무 짧으면 안 읽힐 수도 있음
-
-        // then
-        verify(agentSignalService, times(1)).saveSignal(any(AgentSignalRequest.class));
+        String json = "{\"deviceId\":1,\"statusId\":3,\"timeStamp\":\"2025-04-13T12:26:38\"}";
+        byte[] testData = json.getBytes("UTF-8");
+        DatagramPacket testPacket = new DatagramPacket(testData, testData.length);
+        mockDatagramSocket.injectPacket(testPacket);
+        Thread.sleep(500);
+        AgentSignalRequest expected = new ObjectMapper().readValue(json, AgentSignalRequest.class);
+        verify(mockService, times(1)).saveSignal(refEq(expected));
     }
 }
