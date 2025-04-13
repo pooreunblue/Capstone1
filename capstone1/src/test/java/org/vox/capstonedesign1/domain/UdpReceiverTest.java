209e4224ae4f56f2499466930a8b37de6a32d75d
baekseungho <mjbsh02@mju.ac.kr>
2025-04-14 02:41:11 +0900

    Test: 핸들러에 잘 전달하는지 테스트 수정

--- capstone1/src/test/java/org/vox/capstonedesign1/domain/UdpReceiverTest.java
+++ capstone1/src/test/java/org/vox/capstonedesign1/domain/UdpReceiverTest.java
@@ -1,10 +1,8 @@
 package org.vox.capstonedesign1.domain;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.vox.capstonedesign1.dto.AgentSignalRequest;
-import org.vox.capstonedesign1.service.AgentSignalService;
 import org.vox.capstonedesign1.testutil.MockDatagramSocket;
+import org.vox.capstonedesign1.util.UdpMessageHandler;
 
 import java.net.DatagramPacket;
 
@@ -13,17 +11,16 @@ import static org.mockito.Mockito.*;
 class UdpReceiverTest {
 
     @Test
-    void UDP소켓서비스전달테스트() throws Exception {
-        AgentSignalService mockService = mock(AgentSignalService.class);
-        MockDatagramSocket mockDatagramSocket = new MockDatagramSocket();
-        UdpReceiver receiver = new UdpReceiver(mockService, mockDatagramSocket);
-        receiver.startUdpServer();
-        String json = "{\"deviceId\":1,\"statusId\":3,\"timeStamp\":\"2025-04-13T12:26:38\"}";
-        byte[] testData = json.getBytes("UTF-8");
-        DatagramPacket testPacket = new DatagramPacket(testData, testData.length);
-        mockDatagramSocket.injectPacket(testPacket);
-        Thread.sleep(500);
-        AgentSignalRequest expected = new ObjectMapper().readValue(json, AgentSignalRequest.class);
-        verify(mockService, times(1)).saveSignal(refEq(expected));
+    void UDP메시지_Handler에_전달하는지_테스트() throws Exception {
+        UdpMessageHandler mockHandler = mock(UdpMessageHandler.class);
+        MockDatagramSocket mockSocket = new MockDatagramSocket();
+        UdpReceiver receiver = new UdpReceiver(mockSocket, mockHandler);
+        receiver.start();
+        String message = "{\"deviceId\":1,\"statusId\":3,\"timeStamp\":\"2025-04-13T12:26:38\"}";
+        byte[] testData = message.getBytes("UTF-8");
+        DatagramPacket packet = new DatagramPacket(testData, testData.length);
+        mockSocket.injectPacket(packet);
+        Thread.sleep(300);
+        verify(mockHandler, times(1)).handleMessage(eq(message));
     }
 }
