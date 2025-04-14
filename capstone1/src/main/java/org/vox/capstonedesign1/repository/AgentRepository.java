0c395190a1e4d7205ad5fe31cf13846902fa9725
baekseungho <mjbsh02@mju.ac.kr>
2025-04-14 19:11:07 +0900

    Feat: AgentRepository DeviceId로 Agent 조회 메서드 구현

--- capstone1/src/main/java/org/vox/capstonedesign1/repository/AgentRepository.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/repository/AgentRepository.java
@@ -3,4 +3,8 @@ package org.vox.capstonedesign1.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.vox.capstonedesign1.domain.Agent;
 
-public interface AgentRepository extends JpaRepository<Agent, Long> {}
+import java.util.Optional;
+
+public interface AgentRepository extends JpaRepository<Agent, Long> {
+    Optional<Agent> findByDevice_DeviceId(Long deviceId);
+}
