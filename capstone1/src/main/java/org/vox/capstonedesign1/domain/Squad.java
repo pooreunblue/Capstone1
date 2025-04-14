6137ef4ed2320cb17d096a8f38ea872baaedbd6b
baekseungho <mjbsh02@mju.ac.kr>
2025-04-14 21:13:09 +0900

    Style: 기본 생성자 어노테이션 추가

--- capstone1/src/main/java/org/vox/capstonedesign1/domain/Squad.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/domain/Squad.java
@@ -1,12 +1,15 @@
 package org.vox.capstonedesign1.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Squad {
 
     @Id
