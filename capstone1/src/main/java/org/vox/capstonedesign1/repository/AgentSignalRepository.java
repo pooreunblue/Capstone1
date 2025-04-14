aed13c15fe5e4c825ba4e4e0abf689587376bb93
baekseungho <mjbsh02@mju.ac.kr>
2025-04-14 19:04:41 +0900

    Refactor: 시간 내림차순으로 디바이스 Id를 이용하여 최신 데이터 가져오는 메서드 생성

--- capstone1/src/main/java/org/vox/capstonedesign1/repository/AgentSignalRepository.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/repository/AgentSignalRepository.java
@@ -4,12 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.vox.capstonedesign1.domain.AgentSignal;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AgentSignalRepository extends JpaRepository<AgentSignal, Long> {
-//  CrudRepository에 구현되어있음
-//  AgentSignal save(AgentSignal signal);
-//  Optional<AgentSignal> findById(Long id);
-    List<AgentSignal> findAllByDevice_DeviceId(Long deviceId);
-    // 데이터를 최신시간순으로 정렬해서 가져오기
-    List<AgentSignal> findAllByOrderByTimeStampDesc();
+    Optional<AgentSignal> findTopByDevice_DeviceIdOrderByTimeStampDesc(Long deviceId);
 }
--- capstone1/src/main/java/org/vox/capstonedesign1/service/AgentSignalService.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/service/AgentSignalService.java
@@ -2,10 +2,13 @@ package org.vox.capstonedesign1.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.vox.capstonedesign1.domain.Agent;
 import org.vox.capstonedesign1.domain.AgentSignal;
 import org.vox.capstonedesign1.domain.Device;
 import org.vox.capstonedesign1.domain.EstimatedStatus;
 import org.vox.capstonedesign1.dto.AgentSignalRequest;
+import org.vox.capstonedesign1.dto.AgentSignalResponse;
+import org.vox.capstonedesign1.repository.AgentRepository;
 import org.vox.capstonedesign1.repository.AgentSignalRepository;
 import org.vox.capstonedesign1.repository.DeviceRepository;
 import org.vox.capstonedesign1.repository.EstimatedStatusRepository;
@@ -13,39 +16,63 @@ import org.vox.capstonedesign1.repository.EstimatedStatusRepository;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class AgentSignalService {
 
     private final AgentSignalRepository agentSignalRepository;
+    private final AgentRepository agentRepository;
     private final DeviceRepository deviceRepository;
     private final EstimatedStatusRepository estimatedStatusRepository;
 
-    public AgentSignal saveSignal(AgentSignalRequest request) {
-        Device device = deviceRepository.findById(request.getDeviceId())
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 디바이스 ID"));
-        EstimatedStatus estimatedStatus = estimatedStatusRepository.findById(request.getStatusId())
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상황 ID"));
+    public void saveSignal(AgentSignalRequest request) {
+        Device device = fetchDeviceById(request);
+        EstimatedStatus estimatedStatus = fetchStatusById(request);
         LocalDateTime timeStamp = LocalDateTime.parse(request.getTimeStamp(), DateTimeFormatter.ISO_DATE_TIME);
         AgentSignal agentSignal = AgentSignal.builder()
                 .device(device)
                 .estimatedStatus(estimatedStatus)
                 .timeStamp(timeStamp)
                 .build();
-        return agentSignalRepository.save(agentSignal);
+        agentSignalRepository.save(agentSignal);
     }
 
-    public Optional<AgentSignal> findById(Long id) {
-        return agentSignalRepository.findById(id);
+    public AgentSignalResponse getLatestSignalByDeviceId(Long deviceId) {
+        AgentSignal latestSignal = agentSignalRepository
+                .findTopByDevice_DeviceIdOrderByTimeStampDesc(deviceId)
+                .orElseThrow(() -> new IllegalArgumentException("deviceId" + deviceId + "의 데이터가 없습니다."));
+        Agent agent = agentRepository
+                .findByDevice_DeviceId(deviceId)
+                .orElseThrow(() -> new IllegalArgumentException("deviceID" + deviceId + "에 연결된 agent가 없습니다."));
+        return AgentSignalResponse.builder()
+                .agentName(agent.getAgentName())
+                .estimatedStatus(latestSignal.getEstimatedStatus().getStatusInformation())
+                .deviceName(latestSignal.getDevice().getDeviceName())
+                .timeStamp(latestSignal.getTimeStamp())
+                .streamingFrequency(latestSignal.getStreamingFrequency())
+                .serverIp("192.168.0.0")
+                .build();
     }
 
-    public List<AgentSignal> findAllByDeviceId(Long deviceId) {
-        return agentSignalRepository.findAllByDevice_DeviceId(deviceId);
+    // 여러 명의 최신 상태 조회 (ex: 최대 6명)
+    public List<AgentSignalResponse> getLatestSignalsForDevices(List<Long> deviceIds) {
+        return deviceIds.stream()
+                .map(this::getLatestSignalByDeviceId)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
     }
 
-    public List<AgentSignal> findAllSortedInDescendingOrder() {
-        return agentSignalRepository.findAllByOrderByTimeStampDesc();
+    private Device fetchDeviceById(AgentSignalRequest request) {
+        return deviceRepository.findById(request.getDeviceId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 디바이스 ID"));
+    }
+
+    private EstimatedStatus fetchStatusById(AgentSignalRequest request) {
+        return estimatedStatusRepository.findById(request.getStatusId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상황 ID"));
     }
 }
