ad75f595e71334903d8850359e4d704b4260b08a
baekseungho <mjbsh02@mju.ac.kr>
2025-04-15 16:18:26 +0900

    Feat: 소대ID로 소속 요원들 찾는 JPA 메서드 추가

--- capstone1/src/main/java/org/vox/capstonedesign1/repository/AgentRepository.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/repository/AgentRepository.java
@@ -3,8 +3,10 @@ package org.vox.capstonedesign1.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.vox.capstonedesign1.domain.Agent;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AgentRepository extends JpaRepository<Agent, Long> {
     Optional<Agent> findByDevice_DeviceId(Long deviceId);
+    List<Agent> findBySquad_SquadId(Long squadId);
 }
