de06b3d290a0051d20b81cb8096d2965f3593e5e
baekseungho <mjbsh02@mju.ac.kr>
2025-04-18 23:03:13 +0900

    Refactor: SquadListViewResponse 필드, 생성자 수정

--- capstone1/src/main/java/org/vox/capstonedesign1/dto/SquadListViewResponse.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/dto/SquadListViewResponse.java
@@ -1,16 +1,18 @@
 package org.vox.capstonedesign1.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.vox.capstonedesign1.domain.Squad;
 
-import java.util.List;
-
-@AllArgsConstructor
 @Builder
 @Getter
 public class SquadListViewResponse {
 
-    private String squadName;
-    private List<String> agentNames;
+    private final Long squadId;
+    private final String squadName;
+
+    public SquadListViewResponse(Squad squad) {
+        this.squadId = squad.getSquadId();
+        this.squadName = squad.getSquadName();
+    }
 }
