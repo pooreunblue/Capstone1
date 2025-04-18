588371dfb7a4bd737494922b3453fe485e53acf1
baekseungho <mjbsh02@mju.ac.kr>
2025-04-18 23:17:42 +0900

    Refactor: SquadService findAllWithAgents 메서드 삭제

--- capstone1/src/main/java/org/vox/capstonedesign1/service/SquadService.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/service/SquadService.java
@@ -2,12 +2,10 @@ package org.vox.capstonedesign1.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.vox.capstonedesign1.domain.Agent;
-import org.vox.capstonedesign1.dto.SquadListViewResponse;
+import org.vox.capstonedesign1.domain.Squad;
 import org.vox.capstonedesign1.repository.SquadRepository;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,19 +13,8 @@ public class SquadService {
 
     private final SquadRepository squadRepository;
 
-    public List<SquadListViewResponse> getAllSquadsWithAgents() {
-        return squadRepository.findAllWithAgents()
-                .stream()
-                .map(squad -> {
-                    List<String> agentNames = squad.getAgents().stream()
-                            .map(Agent::getAgentName)
-                            .collect(Collectors.toList());
-                    return SquadListViewResponse.builder()
-                            .squadName(squad.getSquadName())
-                            .agentNames(agentNames)
-                            .build();
-                })
-                .collect(Collectors.toList());
+    public List<Squad> findAll() {
+        return squadRepository.findAll();
     }
 }
 
