52ba29b122306c143b0569ac9d614a099df11a43
baekseungho <mjbsh02@mju.ac.kr>
2025-04-19 20:02:54 +0900

    Fix: 명명오류 수정

--- capstone1/src/main/java/org/vox/capstonedesign1/repository/AgentRepository.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/repository/AgentRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface AgentRepository extends JpaRepository<Agent, Long> {
-    Optional<Agent> findByAgentIdAndSquad_SquadId(Long squadId, Long agentId);
+    Optional<Agent> findBySquad_SquadIdAndAgentId(Long squadId, Long agentId);
     Optional<Agent> findByDevice_DeviceId(Long deviceId);
     List<Agent> findBySquad_SquadIdOrderByAgentIdAsc(Long squadId);
 }
