d8d45b5f3733c3b50739da851182f9d1503975df
baekseungho <mjbsh02@mju.ac.kr>
2025-04-28 17:30:32 +0900

    Feat: 메인 페이지 뷰 구현

--- capstone1/src/main/java/org/vox/capstonedesign1/controller/HomeController.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/controller/HomeController.java
@@ -1,13 +1,13 @@
 package org.vox.capstonedesign1.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/main")
     public String home() {
-        return "Welcome to Vox";
+        return "mainPage";
     }
 }
