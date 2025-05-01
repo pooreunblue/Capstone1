40a068d65bebbc0c5568bc76a5a6c4282c5f53d1
pooreunblue <mjbsh02@mju.ac.kr>
2025-05-01 19:55:32 +0900

    Feat: deviceIdWord로 Device 조회 기능 추가

--- capstone1/src/main/java/org/vox/capstonedesign1/repository/DeviceRepository.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/repository/DeviceRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.vox.capstonedesign1.domain.Device;
 
+import java.util.Optional;
+
 @Repository
 public interface DeviceRepository extends JpaRepository<Device, Long> {
+    Optional<Device> findByDeviceIdWord(String deviceIdWord);
 }
