bcc441aa3a8450b5db8cbe861c03ffd14089a749
pooreunblue <mjbsh02@mju.ac.kr>
2025-05-06 18:04:37 +0900

    Feat: 단일 요원 유니티 조회 api

---
+++ capstone1/src/main/java/org/vox/capstonedesign1/controller/UnityViewController.java
@@ -0,0 +1,15 @@
+package org.vox.capstonedesign1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class UnityViewController {
+    @GetMapping("/unity-view/{deviceSerialNumber}")
+    public String renderUnityView(@PathVariable String deviceSerialNumber, Model model) {
+        model.addAttribute("deviceSerialNumber", deviceSerialNumber);
+        return "unity-view"; // Unity WebGL iframe 페이지 (예: templates/unity-view.html)
+    }
+}
