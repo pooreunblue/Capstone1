907f4009350689dce8b3dcc9e917313593af315a
pooreunblue <mjbsh02@mju.ac.kr>
2025-05-17 02:32:46 +0900

    Refactor: UdpReceiver 수신 형식, 방법 수정

--- capstone1/src/main/java/org/vox/capstonedesign1/domain/UdpReceiver.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/domain/UdpReceiver.java
@@ -8,6 +8,8 @@ import org.vox.capstonedesign1.util.handler.UdpMessageHandler;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 @Slf4j
 @Component
@@ -26,15 +28,22 @@ public class UdpReceiver {
 
     private void listen() {
         log.info("UDP 서버 시작됨. 포트: {}", datagramSocket.getLocalPort());
-        byte[] buffer = new byte[1024];
+        byte[] buffer = new byte[16834];
 
         while (!datagramSocket.isClosed()) {
             try {
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
                 datagramSocket.receive(packet);
-                String message = new String(packet.getData(), 0, packet.getLength(), "UTF-8");
-                log.info("수신된 UDP 데이터: {}", message);
-                messageHandler.handleMessage(message);
+                // ▷ 바이너리 float[] 해석
+                ByteBuffer byteBuffer = ByteBuffer.wrap(packet.getData(), 0, packet.getLength());
+                byteBuffer.order(ByteOrder.LITTLE_ENDIAN); // Python 쪽과 byte 순서 맞추기
+                int numFloats = packet.getLength() / 4;
+                float[] data = new float[numFloats];
+                for (int i = 0; i < numFloats; i++) {
+                    data[i] = byteBuffer.getFloat();
+                }
+                log.info("수신된 UDP 데이터(float[]) 길이: {}", data.length);
+//                messageHandler.handleMessage(message);
             } catch (Exception e) {
                 log.error("UDP 수신 중 오류 발생", e);
             }
