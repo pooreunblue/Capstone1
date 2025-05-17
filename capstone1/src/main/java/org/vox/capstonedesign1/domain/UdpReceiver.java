3780fffd65081acc9147e8392b38a2e316ae950a
pooreunblue <mjbsh02@mju.ac.kr>
2025-05-18 01:34:15 +0900

    Refactor: 코드 리팩토링

--- capstone1/src/main/java/org/vox/capstonedesign1/domain/UdpReceiver.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/domain/UdpReceiver.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.vox.capstonedesign1.util.handler.AgentSignalHandler;
-import org.vox.capstonedesign1.util.handler.UdpMessageHandler;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
--- capstone1/src/main/java/org/vox/capstonedesign1/service/UdpSocketConfig.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/service/UdpSocketConfig.java
@@ -4,13 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 
 @Configuration
 public class UdpSocketConfig {
 
     @Bean
-    public DatagramSocket datagramSocket() throws SocketException {
-        return new DatagramSocket(9999);
+    public DatagramSocket datagramSocket() throws SocketException, UnknownHostException {
+        return new DatagramSocket(9999, InetAddress.getByName("0.0.0.0"));
     }
 }
