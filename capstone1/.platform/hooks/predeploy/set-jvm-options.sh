6af9f3a75555e7c233520888fe84efca21c595b1
pooreunblue <mjbsh02@mju.ac.kr>
2025-05-19 23:28:21 +0900

    Build: 빌드 설정 수정

--- capstone1/.platform/hooks/predeploy/set-jvm-options.sh
+++ capstone1/.platform/hooks/predeploy/set-jvm-options.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo 'JAVA_TOOL_OPTIONS="-Djava.net.preferIPv4Stack=true"' > /etc/profile.d/java-options.sh
+echo 'export JAVA_TOOL_OPTIONS="-Djava.net.preferIPv4Stack=true"' > /etc/profile.d/java-options.sh
 chmod +x /etc/profile.d/java-options.sh
