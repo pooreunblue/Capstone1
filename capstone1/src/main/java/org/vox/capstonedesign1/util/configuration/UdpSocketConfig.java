151565ed536d1c3c8cdba525668bc092d8e7e8c8
pooreunblue <mjbsh02@mju.ac.kr>
2025-05-26 00:41:34 +0900

    Refactor: 코드 리팩토링

--- capstone1/src/main/java/org/vox/capstonedesign1/util/configuration/UdpSocketConfig.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/util/configuration/UdpSocketConfig.java
@@ -3,16 +3,13 @@ package org.vox.capstonedesign1.util.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.net.*;
 
 @Configuration
 public class UdpSocketConfig {
 
     @Bean
     public DatagramSocket datagramSocket() throws SocketException, UnknownHostException {
-        return new DatagramSocket(9999, InetAddress.getByName("0.0.0.0"));
+        return new DatagramSocket(new InetSocketAddress("0.0.0.0", 9999));
     }
 }
