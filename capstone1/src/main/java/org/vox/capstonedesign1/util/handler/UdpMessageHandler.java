ecfee1ee5f50a658aad18ace7e1ae41524c120d8
pooreunblue <mjbsh02@mju.ac.kr>
2025-05-25 22:07:31 +0900

    Refactor: UdpMessageHandler 메소드 인자 수정

--- capstone1/src/main/java/org/vox/capstonedesign1/util/handler/UdpMessageHandler.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/util/handler/UdpMessageHandler.java
@@ -1,5 +1,5 @@
 package org.vox.capstonedesign1.util.handler;
 
 public interface UdpMessageHandler {
-    void handleMessage(float[] message) throws Exception;
+    void handleMessage(byte[] message);
 }
