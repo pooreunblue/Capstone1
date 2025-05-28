de57b64dc40fda9a05690b01cc119ecc1136586d
pooreunblue <mjbsh02@mju.ac.kr>
2025-05-29 00:34:29 +0900

    Test: DeviceService 생성 및 구현

---
+++ capstone1/src/test/java/org/vox/capstonedesign1/service/DeviceServiceTest.java
@@ -0,0 +1,42 @@
+package org.vox.capstonedesign1.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.vox.capstonedesign1.util.handler.AgentSignalHandler;
+
+import java.net.DatagramSocket;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class DeviceServiceTest {
+
+    @MockBean
+    private DatagramSocket datagramSocket;
+
+    @MockBean
+    private UdpReceiveService udpReceiveService;
+
+    @MockBean
+    private AgentSignalHandler agentSignalHandler;
+
+    @Autowired
+    private DeviceService deviceService;
+
+    @Test
+    void 디바이스시리얼넘버반환테스트() {
+
+        //given
+        Long deviceId = 1L;
+        String expected = "abc123";
+
+        //when
+        String serialNumber = deviceService.resolveDeviceSerialNumber(deviceId);
+
+        //then
+        assertThat(serialNumber).isNotNull();
+        assertThat(serialNumber).isEqualTo(expected);
+    }
+}
