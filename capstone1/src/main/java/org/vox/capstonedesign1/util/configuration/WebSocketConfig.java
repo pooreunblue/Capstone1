526db51a026fba0a9af28666b998f2bbcaa8fc3a
pooreunblue <mjbsh02@mju.ac.kr>
2025-06-02 17:45:26 +0900

    Refactor: 코드 리팩토링

--- capstone1/src/main/java/org/vox/capstonedesign1/util/configuration/WebSocketConfig.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/util/configuration/WebSocketConfig.java
@@ -17,7 +17,6 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(unityWebSocketHandler, "/ws/unity")
-                .setAllowedOrigins("https://voxkr.xyz")
-                .withSockJS();
+                .setAllowedOrigins("https://voxkr.xyz");
     }
 }
