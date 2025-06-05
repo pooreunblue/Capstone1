e6eb26e85cad3f56fd414ed2d355f826745e40c9
pooreunblue <mjbsh02@mju.ac.kr>
2025-06-05 21:10:52 +0900

    Refactor: 코드 리팩토링

---
+++ capstone1/src/main/java/org/vox/capstonedesign1/controller/StreamController.java
@@ -0,0 +1,15 @@
+package org.vox.capstonedesign1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class StreamController {
+    @GetMapping("/main/squads/{squadId}/{serial}/hls")
+    public String hlsPlayer(@PathVariable Long squadId, @PathVariable String serial, Model model) {
+        model.addAttribute("streamKey", serial);
+        return "hlsPlayer";
+    }
+}
