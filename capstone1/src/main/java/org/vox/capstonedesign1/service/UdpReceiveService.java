15611204c5e241a10f0cb073947a05d919d91f63
pooreunblue <mjbsh02@mju.ac.kr>
2025-06-07 21:22:29 +0900

    Style: UDP 테스트를 위한 주석화

--- capstone1/src/main/java/org/vox/capstonedesign1/service/UdpReceiveService.java
+++ capstone1/src/main/java/org/vox/capstonedesign1/service/UdpReceiveService.java
@@ -34,7 +34,7 @@ public class UdpReceiveService {
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
                 datagramSocket.receive(packet);
                 byte[] data = Arrays.copyOfRange(packet.getData(), 0, packet.getLength());
-                agentSignalHandler.handleMessage(data);
+                // agentSignalHandler.handleMessage(data);
                 log.info("수신된 UDP 데이터(float[]) 길이: {}", data.length / 4);
             } catch (IOException e) {
                 log.error("UDP 수신 중 오류 발생", e);
